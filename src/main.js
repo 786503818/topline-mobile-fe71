eab950a9acbee05fe4c0cef6a8a1ac161cade24d
wang <wang@itcast.cn>
2019-07-08 21:43:04 +0800

    配置REM适配

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue'// 导入vue
+import App from './App.vue'// 导入根组件
+import router from './router'// 导入路由模块
 import store from './store'
-
+import Vant from 'vant' // 导入组件库
+import 'vant/lib/index.css'// 导入样式
+import 'amfe-flexible' // 导入适配
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
